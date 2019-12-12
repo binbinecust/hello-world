51412ed429935394f758495f10a2e12d1a2e2b06
binbinfang <binbinfang@sohu-inc.com>
2019-12-12 14:30:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       alt="Vue logo"
       src="./assets/logo.png"
     >
-    <HelloWorld msg="Welcome to Your Vue.js App hahah" />
+    <HelloWorld msg="Welcome to Your Vue.js App lalalal" />
   </div>
 </template>
 
